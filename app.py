d98de072af8af55132381afd70ddd465d0ab28c7
David Piccolella <davepiccolella@gmail.com>
2016-03-26 16:00:15 -0400

    Adding search capability

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ from flask.ext.login import LoginManager, login_user, logout_user, login_require
 from flask.ext.mongoengine.wtf import model_form
 from wtforms import PasswordField
 
-import requests
+
+import requests.packages.urllib3
+#requests.packages.urllib3.disable_warnings()
 
 app = Flask(__name__)
 
@@ -13,20 +15,20 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 app.config["DEBUG"] = True
-app.config['MONGODB_SETTINGS'] = {'db' : 'books'}
+app.config['MONGODB_SETTINGS'] = {'db' : 'events'}
 app.config['SECRET_KEY'] = 'ajosjdfajjjj3453453oj!!!oij'
 app.config['WTF_CSRF_ENABLED'] = True
 
 db = MongoEngine(app)
 
-class User(db.Document):
+class newUser(db.Document):
 
 	name = db.StringField(required=True, unique=True)
 	password = db.StringField(required=True)
 
 	def is_authenticated(self):
 		
-		users = User.objects(name=self.name, password=self.password)
+		users = newUser.objects(name=self.name, password=self.password)
 		return len(users) != 0
 
 	def is_active(self):
@@ -43,7 +45,7 @@ class User(db.Document):
 @login_manager.user_loader
 def load_user(name):
 	
-	users = User.objects(name=name)
+	users = newUser.objects(name=name)
 
 	if len(users) != 0:
 		return users[0]
@@ -51,40 +53,75 @@ def load_user(name):
 	else:
 		return None
 
-UserForm = model_form(User)
+UserForm = model_form(newUser)
 UserForm.password = PasswordField('password')
 
-class FavoriteBook(db.Document):
-	author = db.StringField(required=True)
+class FavoriteEvent(db.Document):
 	title = db.StringField(required=True)
+	date_time = db.StringField(required=True)
+	location = db.StringField(required=True)
+	event_id = db.StringField(required=True)
 	link = db.StringField(required=True)
-	poster = db.ReferenceField(User)
+	poster = db.ReferenceField(newUser)
 
 
 
 @app.route("/")
-
 def hello():
 
 	return render_template("hello.html", current_user=current_user)
 
 
-@app.route("/name")
-
-def name():
-
-	return "Dave"
 
 @app.route("/search", methods=["POST", "GET"])
 
 @login_required
 def search():
-    if request.method == "POST":
-        url = "https://www.googleapis.com/books/v1/volumes?q=" + request.form["user_search"]
-        response_dict = requests.get(url).json()
-        return render_template("results.html", api_data=response_dict)
-    else: # request.method == "GET"
-        return render_template("search.html")
+	if request.method == "POST":
+		total = 0
+		date_list= []
+		time_list = []
+
+		if request.form["other_search"]:
+			url = "https://api.seatgeek.com/2/events?venue.state=" + request.form["other_search"] + "&q=" + request.form["user_search"] + "&client_id=NDM5NTU0NHwxNDU4NzUzODgz"
+        
+		else:
+			url = "https://api.seatgeek.com/2/events?" + "q=" + request.form["user_search"] + "&client_id=NDM5NTU0NHwxNDU4NzUzODgz"
+		
+		response_dict = requests.get(url).json()
+		print response_dict
+		total = response_dict["meta"]["total"]
+		url = url + "&per_page=" + str(total)
+		response_dict = requests.get(url).json()
+
+		for event in response_dict["events"]:
+			date_str = ""
+			time_str = ""
+			mystr = ""
+			temp_list=[]
+			mystr = event["datetime_local"]
+			mystr = mystr.replace('-', ' ')
+			mystr = mystr.replace('T', ' ')
+			temp_list = (mystr.split(' '))
+			date_str= temp_list[1] + "/" + temp_list[2] + "/" + temp_list[0] + " "
+
+			if int(temp_list[3][:2]) > 12:
+
+				time_str =  str(int(temp_list[3][:2])-12) + temp_list[3][2:] + " PM"
+
+			else:
+
+				time_str = temp_list[3] + " AM"
+       
+			date_list.append(date_str)
+			time_list.append(time_str)
+
+        	
+		num_events= len(date_list)
+		print url
+		return render_template("results.html", api_data=response_dict, time_list=time_list, date_list=date_list, num_events=num_events)
+	else: 
+		return render_template("search.html")
 
 
 @app.route("/favorite/<id>")
@@ -93,7 +130,7 @@ def search():
 def favorite(id):
 	book_url = "https://www.googleapis.com/books/v1/volumes/" + id
 	book_dict = requests.get(book_url).json()
-	poster = User.objects(name=current_user.name).first()
+	poster = newUser.objects(name=current_user.name).first()
 	new_fav = FavoriteBook(author=book_dict["volumeInfo"]["authors"][0], title=book_dict["volumeInfo"]["title"], link=book_url, poster=poster)
 	new_fav.save()
 	return render_template("confirm.html", api_data=book_dict)
@@ -103,7 +140,7 @@ def favorite(id):
 @login_required
 def delete_favorite(id):
 	
-	current_poster = User.objects(name=current_user.name).first()
+	current_poster = newUser.objects(name=current_user.name).first()
 	FavoriteBook.objects(poster=current_poster, title=id).delete()
 
 	return redirect("/favorites")
@@ -113,7 +150,7 @@ def delete_favorite(id):
 def favorites():
 
 
-	current_poster = User.objects(name=current_user.name).first()
+	current_poster = newUser.objects(name=current_user.name).first()
 	favorites = FavoriteBook.objects(poster=current_poster)
 	return render_template("favorites.html", current_user = current_user, favorites=favorites)
 
@@ -138,11 +175,11 @@ def login():
 
 	if request.method == 'POST' and form.validate():
 
-		users = User.objects(name=form.name.data, password=form.password.data)
+		users = newUser.objects(name=form.name.data, password=form.password.data)
 		
 		if len(users) != 0:
 
-			user = User(name=form.name.data, password=form.password.data)
+			user = newUser(name=form.name.data, password=form.password.data)
 			login_user(user)
 			return redirect('/')
 
